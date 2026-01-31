287c83e34ba4729615bc333d603f23ef50377f0b
Niklas Rammerstorfer <rammerstorfer.n@gmail.com>
2026-01-31 11:22:22 +0100

    Fixed layouting.

--- app/src/main/java/com/ramniksoftware/bullseye/GameScreen.kt
+++ app/src/main/java/com/ramniksoftware/bullseye/GameScreen.kt
@@ -1,38 +1,65 @@
 package com.ramniksoftware.bullseye
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ramniksoftware.bullseye.ui.theme.BullseyeTheme
 
 @Composable
 fun GameScreen() {
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
     ) {
-        Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-        Text("89", fontSize = 32.sp, fontWeight = FontWeight.Bold)
-        Row(
-            verticalAlignment = Alignment.CenterVertically
+        Spacer(modifier = Modifier.weight(.5f))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.weight(9f)
         ) {
-            Text("1")
-            Slider(
-                value = 0.5f,
-                valueRange = 0.01f..1f,
-                onValueChange = {}
-            )
-            Text("100")
+            Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+            Text("89", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    "1",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+                Slider(
+                    value = 0.5f,
+                    valueRange = 0.01f..1f,
+                    onValueChange = {},
+                    modifier = Modifier.weight(1f)
+                )
+                Text(
+                    "100",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(end = 16.dp))
+            }
+            Button(onClick = {}) {
+                Text("HIT ME")
+            }
         }
-        Button(onClick = {}) {
-            Text("HIT ME")
-        }
+        Spacer(modifier = Modifier.weight(.5f))
     }
 }
 
