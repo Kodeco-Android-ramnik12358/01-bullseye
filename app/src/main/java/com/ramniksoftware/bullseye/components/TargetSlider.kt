025e1ecb57ca595f99705bf6b733eecd923b8707
Niklas Rammerstorfer <rammerstorfer.n@gmail.com>
2026-02-01 18:27:47 +0100

    Styled slider.

--- app/src/main/java/com/ramniksoftware/bullseye/components/TargetSlider.kt
+++ app/src/main/java/com/ramniksoftware/bullseye/components/TargetSlider.kt
@@ -1,22 +1,33 @@
 package com.ramniksoftware.bullseye.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.ramniksoftware.bullseye.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TargetSlider(
     modifier: Modifier = Modifier,
     value: Float = 0.5f,
-    onValueChange: (Float) -> Unit) {
+    onValueChange: (Float) -> Unit
+) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
@@ -30,11 +41,30 @@ fun TargetSlider(
             value = value,
             valueRange = 0.01f..1f,
             onValueChange = onValueChange,
-            modifier = Modifier.weight(1f)
+            modifier = Modifier.weight(1f),
+            thumb = {
+                Image(
+                    modifier = Modifier.size(100.dp),
+                    painter = painterResource(R.drawable.nub),
+                    contentDescription = stringResource(R.string.slider_thumb_description)
+                )
+            },
+            track = {
+                Box(
+                    modifier = Modifier
+                        .height(8.dp)
+                        .fillMaxWidth()
+                        .background(
+                            color = MaterialTheme.colorScheme.primary,
+                            shape = MaterialTheme.shapes.small
+                        )
+                )
+            }
         )
         Text(
             stringResource(R.string.max_value_text),
             textAlign = TextAlign.Center,
-            modifier = Modifier.padding(end = 16.dp))
+            modifier = Modifier.padding(end = 16.dp)
+        )
     }
 }
